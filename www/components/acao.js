8051afe442e954effbd33f2a6565c02042c3aa44
Assilem05 <melissafranca.oliveira@gmail.com>
2020-08-21 12:21:02 +0900

    atualização do projeto - 2.0

--- www/components/acao.js
+++ www/components/acao.js
@@ -3,7 +3,7 @@
 $(documents).on("click","#buscar",function(){
     $.ajax({
       type:"get", //como vou enviar
-      url:"http://viacep.com.br/ws/"+$("#cep").val()+"/json/", //para onde enviar
+      url:"http://viacep.com.br/ws/"+$("#regiao").val()+"/json/", //para onde enviar
       success: function(data){ //executar se der certo
         var conteudo;
         conteudo = "<p><strong>Logradouro:</strong>"+data.logradouro+"</p>";
